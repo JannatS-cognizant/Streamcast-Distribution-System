e4e73f3a27141307935f4e38fca93ee4355653ff
swas-06 <Swastika.Dwibedi@cognizant.com>
2026-05-24 22:46:01 +0530

    Delete Final Microservice/Streamcast-Backend directory

--- Final Microservice/Streamcast-Backend/src/main/java/com/cts/streamcast/repository/RoleRepository.java
+++
@@ -1,9 +0,0 @@
-package com.cts.streamcast.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.cts.streamcast.entity.Role;
-
-public interface RoleRepository extends JpaRepository<Role, Long>{
-
-}
--- Final Microservice/Streamcast-Backend/src/main/java/com/cts/streamcast/service/EmailService.java
+++
@@ -1,5 +0,0 @@
-package com.cts.streamcast.service;
-
-public interface EmailService {
-    void sendEmail(String to, String subject, String body);
-}
